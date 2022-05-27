6c7c7580a426588de7e929d27701378210541883
Donte Ladatto <donte279@gmail.com>
2022-05-27 16:35:39 -0500

    commit tree 6fa1cf47ad318e79be94d0a60cf14f98dd38ba1d

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,8 @@
-# Write your code here.
+katz_deli = []
+
+def line(arr)
+    if arr == []
+        puts "The line is currently empty."
+    else puts "The line is currently: " + arr.join(" ")
+    end
+end
